b4cf0ea7f48925eb9adf947a30cd3370546d8d1a
hong kun <k@qq.com>
2020-04-21 12:50:13 +0800

    自定义注入

---
+++ dependency-injection/src/main/java/com/think/in/spring/dependency/injection/SelfDefineInjection.java
@@ -0,0 +1,15 @@
+package com.think.in.spring.dependency.injection;
+
+/**
+ * @description: 自定义依赖注入注解，根据源码分析之后要自定义依赖注入注解，
+ * 相当于{@link org.springframework.beans.factory.annotation.Autowired}
+ * 生命周期处理{@link org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor}
+ * {@link org.springframework.beans.factory.support.MergedBeanDefinitionPostProcessor}
+ * 元数据{@link org.springframework.beans.factory.annotation.InjectionMetadata}
+ * {@link org.springframework.beans.factory.annotation.InjectionMetadata.InjectedElement}
+ * @author: H.K
+ * @program: thinking-in-spring
+ * @create: 2020-04-15 23:09
+ */
+public class SelfDefineInjection {
+}
